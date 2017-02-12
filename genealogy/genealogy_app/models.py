603fe0395f94b72d1276e2bc6bdaab6edad34264
Marcin Brzescianski <Marcin.brzescianski@gmail.com>
2017-02-12 23:12:05 +0100

    add model anceator

--- genealogy/genealogy_app/models.py
+++ genealogy/genealogy_app/models.py
@@ -3,5 +3,15 @@ from django import forms
 
 # Create your models here.
 
+class Anceator(models.model):
+    first_name = models.CharField(max_length=24)
+    secound_name = models.CharField(max_length=24, null = True)
+    surname = models.CharField(max_length=36)
+    maiden_name = models.CharField(max_length=36)
+    relationship = models.ManyToManyField('relationship', through='AnceatorRelationship')
 
-    
+    def __str__(self):
+        return '{} {} {}'.format(self.first_name, self.secound_name, self.surname)
+
+class Relationship(models.Model):
+    pass
