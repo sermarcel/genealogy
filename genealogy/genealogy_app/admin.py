807f9cddb39e076d062a6f8c257e64b6433c6d14
Marcin Brzescianski <Marcin.brzescianski@gmail.com>
2017-02-17 23:52:38 +0100

    add to admin

--- genealogy/genealogy_app/admin.py
+++ genealogy/genealogy_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from genealogy_app.models import Anceator
 
 # Register your models here.
+
+@admin.register(Anceator)
+class AnceatorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'surname')
