7c261d4e9cce19e5c8c95e8acbef64bd8bbe74ae
Marcin Brzescianski <Marcin.brzescianski@gmail.com>
2017-02-18 23:32:32 +0100

    models

--- genealogy/genealogy_app/models.py
+++ genealogy/genealogy_app/models.py
@@ -20,6 +20,8 @@ class Anceator(models.Model):
     surname = models.CharField(max_length=36)
     maiden_name = models.CharField(max_length=36)
     relationship = models.ManyToManyField('relationship', through='AnceatorRelationship')
+    notes = models.TextField()
+    image = models.ImageField(upload_to='static/content/', null=True, blank=True)
 
     def __str__(self):
         return '{} {} {}'.format(self.first_name, self.secound_name, self.surname)
